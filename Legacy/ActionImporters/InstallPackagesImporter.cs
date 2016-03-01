f3d528d03a2da229eb58bdab3cf3213b60b118d6
Karl Harnagy <kharnagy@inedo.com>
2016-03-01 10:53:43 -0500

    Moved actions to legacy.

---
+++ Legacy/ActionImporters/InstallPackagesImporter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Inedo.BuildMaster.Extensibility.Operations;
+using Inedo.BuildMasterExtensions.NuGet.Operations;
+using Inedo.IO;
+
+namespace Inedo.BuildMasterExtensions.NuGet.Legacy.ActionImporters
+{
+    internal sealed class InstallPackagesImporter : IActionOperationConverter<InstallSolutionPackagesAction, InstallPackagesOperation>
+    {
+        public ConvertedOperation<InstallPackagesOperation> ConvertActionToOperation(InstallSolutionPackagesAction action, IActionConverterContext context)
+        {
+            return new InstallPackagesOperation
+            {
+                SourceDirectory = AH.NullIf(action.OverriddenSourceDirectory, string.Empty),
+                PackageOutputDirectory = AH.CoalesceString(action.PackageOutputDirectory, "packages")
+            };
+        }
+    }
+}
