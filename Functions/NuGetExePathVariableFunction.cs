8f3d81c69af28492caa001cc2a90c83adccae2e0
kharnagy <kharnagy@inedo.com>
2016-11-22 14:18:37 -0500

    v5.6 SDK

--- Functions/NuGetExePathVariableFunction.cs
+++ Functions/NuGetExePathVariableFunction.cs
@@ -1,13 +1,15 @@
 ﻿using System.ComponentModel;
 using Inedo.BuildMaster.Extensibility;
 using Inedo.BuildMaster.Extensibility.VariableFunctions;
+using Inedo.Documentation;
 
 namespace Inedo.BuildMasterExtensions.NuGet.Functions
 {
     [ScriptAlias("NuGetExePath")]
     [Description("The path to the nuget.exe client, otherwise the included nuget.exe client is used.")]
-    //[ExtensionConfigurationVariable(Required = false)]
-    public sealed class NuGetExePathVariableFunction : ScalarVariableFunctionBase
+    [ExtensionConfigurationVariable(Required = false)]
+    [Tag("nuget")]
+    public sealed class NuGetExePathVariableFunction : ScalarVariableFunction
     {
         protected override object EvaluateScalar(IGenericBuildMasterContext context)
         {
