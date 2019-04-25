bbb270ba10acf9bde132d992c1bdd60d4254b109
Greg D <gdivis@inedo.com>
2019-04-25 10:02:03 -0400

    Add AppliesToAttribute to extension. Fixes #26

--- NuGet/InedoExtension/Properties/AssemblyInfo.cs
+++ NuGet/InedoExtension/Properties/AssemblyInfo.cs
@@ -5,8 +5,10 @@ using Inedo.Extensibility;
 [assembly: AssemblyTitle("NuGet")]
 [assembly: AssemblyDescription("Contains operations for working with NuGet.")]
 [assembly: AssemblyCompany("Inedo, LLC")]
-[assembly: AssemblyCopyright("Copyright © Inedo 2018")]
+[assembly: AssemblyCopyright("Copyright © Inedo 2019")]
 [assembly: AssemblyVersion("1.0.1")]
 [assembly: AssemblyFileVersion("1.0.1")]
 [assembly: ComVisible(false)]
 [assembly: ScriptNamespace("NuGet")]
+
+[assembly: AppliesTo(InedoProduct.BuildMaster | InedoProduct.Otter | InedoProduct.Hedgehog)]
